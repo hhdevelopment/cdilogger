971c66ead08215293a5cb45aa04f574a4f3e8349
hhfrancois <francois.achache@gmail.com>
2016-02-28 12:39:42 +0100

    releaseVersion

--- deployOrPerform.sh
+++ deployOrPerform.sh
@@ -1,5 +1,6 @@
 CURRENT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-echo $CURRENT_VERSION
+echo "CURRENT_VERSION = "$CURRENT_VERSION
+echo "releaseVersion = "$releaseVersion
 # mvn --B -Dtag=${project-name}-${releaseVersion} release:prepare \
-                 -DreleaseVersion=${releaseVersion} \
-                 -DdevelopmentVersion=${developmentVersion}
+#                 -DreleaseVersion=${releaseVersion} \
+#                 -DdevelopmentVersion=${developmentVersion}
