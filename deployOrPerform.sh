1f6f873a0362fcc1016a82529608cfe2b6022719
hhfrancois <francois.achache@gmail.com>
2016-02-28 13:05:50 +0100

    PROJECT_NAME

--- deployOrPerform.sh
+++ deployOrPerform.sh
@@ -1,4 +1,3 @@
-#CURRENT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 CURRENT_VERSION=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}version').text)"`
 echo "CURRENT_VERSION = "$CURRENT_VERSION
 echo "releaseVersion = "$releaseVersion
@@ -7,6 +6,8 @@ if [ "$CURRENT_VERSION" = "$releaseVersion-SNAPSHOT" ]; then
 PERFORM=true
 fi
 echo "PERFORM = "$PERFORM
+PROJECT_NAME=`python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find('{http://maven.apache.org/POM/4.0.0}artifactId').text)"`
+echo "PROJECT_NAME = "$PROJECT_NAME
 # mvn --B -Dtag=${project-name}-${releaseVersion} release:prepare \
 #                 -DreleaseVersion=${releaseVersion} \
 #                 -DdevelopmentVersion=${developmentVersion}
